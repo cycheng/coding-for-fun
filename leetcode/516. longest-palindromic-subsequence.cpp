eb9356a72d65192b2f80e277acf8aa36a1a2310e
Chuang-Yu Cheng <cycheng.buddhist@gmail.com>
2019-12-07 21:58:32 +0000

    516: add another solution

--- leetcode/516. longest-palindromic-subsequence.cpp
+++ leetcode/516. longest-palindromic-subsequence.cpp
@@ -3,7 +3,7 @@
 // #dp, #string
 #include "std.h"
 
-int longestPalindromeSubseq(string s) {
+int longestPalindromeSubseq_64ms(string s) {
   // s[i][j] = s[i+1][j-1]
   // s[i][j] = max(s[i][j-1], s[i+1][j])
   const int n = s.length();
@@ -18,4 +18,22 @@ int longestPalindromeSubseq(string s) {
     }
   }
   return dp[0][n - 1];
+}
+
+int lcs(const string &s, const string &t) {
+  const int m = s.length() + 1, n = t.length() + 1;
+  vector<vector<int>> dp(m, vector<int>(n));
+  for (int i = 1; i < m; ++i)
+    for (int j = 1; j < n; ++j)
+      if (s[i - 1] == t[j - 1])
+        dp[i][j] = dp[i - 1][j - 1] + 1;
+      else
+        dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
+  return dp[m - 1][n - 1];
+}
+
+int longestPalindromeSubseq_116ms(string s) {
+  string t = s;
+  reverse(s.begin(), s.end());
+  return lcs(s, t);
 }
