68eb90f88ef1d49e0f5402dbb8e710b9cda7a31e
Chuang-Yu Cheng <cycheng.buddhist@gmail.com>
2020-02-12 06:32:16 +0000

    [leetcode] 309. best-time-to-buy-and-sell-stock-with-cooldown, add better method 1 solution.

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # coding-for-fun
 Coding for fun.
+
+# Dynamic Programming
+
+- Day 10:
+  leetcode/309. best-time-to-buy-and-sell-stock-with-cooldown.cpp
--- leetcode/309. best-time-to-buy-and-sell-stock-with-cooldown.cpp
+++ leetcode/309. best-time-to-buy-and-sell-stock-with-cooldown.cpp
@@ -6,6 +6,8 @@
 #include "std.h"
 
 // method 1:
+// dp(k) = the maximum profit if we sell stock on kth day.
+//
 // dp(k) = max(prices[k] - prices[x] + dp(0) .. dp(x - 2))
 // x = 0 .. k - 1
 int maxProfit_72ms(vector<int> &prices) {
@@ -24,4 +26,21 @@ int maxProfit_72ms(vector<int> &prices) {
     leftMax[k] = max(leftMax[k - 1], dp[k]);
   }
   return leftMax[n - 1];
+}
+
+int maxProfit_60ms(vector<int> &prices) {
+  if (prices.size() < 2)
+    return 0;
+
+  const int n = prices.size();
+  vector<int> leftMax(n);
+  for (int k = 1; k < n; ++k) {
+    int maxk = 0;
+    for (int x = k - 1; x >= 0; --x) {
+      int prevMax = x >= 2 ? leftMax[x - 2] : 0;
+      maxk = max(maxk, prices[k] - prices[x] + prevMax);
+    }
+    leftMax[k] = max(leftMax[k - 1], maxk);
+  }
+  return leftMax.back();
 }
